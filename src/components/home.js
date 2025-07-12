0f85355343eaed7d8ae13dbe19df8029d6da909f
DhuJUICE <junierherandien84@gmail.com>
2025-07-13 00:26:15 +0200

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import { Typography, Box, Container } from "@mui/material";
 
 const Home = () => {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "75vh" }}>
       <Container sx={{ padding: 2, flexGrow: 1 }}>
         <Box sx={{ padding: 2, bgcolor: "#ffffff", borderRadius: 2 }}>
           <Typography variant="h5">Welcome to the platform</Typography>
